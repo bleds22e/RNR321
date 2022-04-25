a2999820f22ca1d55d5a62cd5fe628150a1c32e5
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2022-04-25 16:13:23 -0700

    make the beads script easier to work with

--- scripts/Distance_bead_data_mine.R
+++ scripts/Distance_bead_data_mine.R
@@ -2,8 +2,9 @@ library(tidyverse)
 library(unmarked) # Load unmarked
 
 # Read distance sampling data for clear glass beads
+# Be sure to use read.csv this time!!
 
-beads <- read_csv("data_raw/bead_data_2021.csv")
+beads <- read.csv("data_raw/bead_data_2021.csv")
 
 str(beads)    # View the structure of the dataframe
 head(beads)   # First few observations
@@ -16,35 +17,42 @@ transect_length <- beads %>%
 
 # Plot the distribution of detection distances
 
-hist(beads$distance, xlab="distance (m)", main="Clear beads", cex.lab=0.8, cex.axis=0.8) 
+hist(beads$distance)
 
-# Based on the hiostogram, I'll suggest we truncate at 5 m
+# Based on the histogram, I'll suggest we truncate at 5 m
 
 # Set truncation distance to eliminate extreme observations
-
 trunc <- 5
 
 # Set 'cut points' for distance bins; every 1 meter
+distance_bins <- seq(0, trunc, by = 1) 
 
-db <- seq(0, trunc, by = 1) 
-
-data <- formatDistData(beads, distCol="distance", transectNameCol="group", dist.breaks=db)
+data <- formatDistData(beads, 
+                       distCol = "distance", 
+                       transectNameCol = "group", 
+                       dist.breaks = distance_bins)
 data
 
 # Assemble data into the format required by unmarked, called an 'unmarked frame'
-
-UMF <- unmarkedFrameDS(y=as.matrix(data), survey="line", tlength = tl$length, 
-                       dist.breaks=db, unitsIn="m")
+UMF <- unmarkedFrameDS(y = as.matrix(data), 
+                       survey = "line", 
+                       tlength = transect_length$length, 
+                       dist.breaks = distance_bins, 
+                       unitsIn = "m")
 UMF
 
 # Check the distribution of detection distances to be used for analysis
 
-hist(UMF, xlab="distance (m)", main="Saguaros < 2m", cex.lab=0.8, cex.axis=0.8) 
+hist(UMF) 
 
-HN   <- distsamp(~1 ~1, UMF, keyfun="halfnorm", output="density", unitsOut="ha")
-HR   <- distsamp(~1 ~1, UMF, keyfun="hazard",   output="density", unitsOut="ha")
-Unif <- distsamp(~1 ~1, UMF, keyfun="uniform",  output="density", unitsOut="ha")
-Exp  <- distsamp(~1 ~1, UMF, keyfun="exp",      output="density", unitsOut="ha")
+HN   <- distsamp(~1 ~1, UMF, keyfun = "halfnorm", 
+                 output = "density", unitsOut = "ha")
+HR   <- distsamp(~1 ~1, UMF, keyfun = "hazard",
+                 output = "density", unitsOut = "ha")
+Unif <- distsamp(~1 ~1, UMF, keyfun = "uniform",
+                 output = "density", unitsOut = "ha")
+Exp  <- distsamp(~1 ~1, UMF, keyfun = "exp", 
+                 output = "density", unitsOut = "ha")
 
 # Model Selection
 
@@ -54,26 +62,27 @@ models <- fitList('Half Normal' = HN,
                   'Exponential' = Exp)
 modSel(models)
 
-# GOF
+### An illustration (*** no need for you to repeat this part ***) of the 
+### the distance data with the Hazard rate function
 
-(gof <- parboot(HN, nsim=100))
-plot(gof)
+hr.shape <- exp(coef(HR, type="det"))
+hr.scale <- exp(coef(HR, type="scale"))
+hist(UMF, xlab="distance (m)", main="Ocotillos", cex.lab=0.8, cex.axis=0.8) 
+par(new=TRUE)
+plot(function(x) gxhaz(x, shape=hr.shape, scale=hr.scale), 0, trunc, xaxt='n', yaxt='n', 
+     ann = FALSE, col = "blue", lwd=2)
 
 # --------   Density estimate   --------- #
 
-backTransform(HN, type="state") # Density estimate (no./ha) 
-exp(confint(HN, type="state"))  # CI for density
+# handy function to get out density and confidence intervals
+backTransform(HN, type = "state") # Density estimate (no./m) 
+exp(confint(HN, type = "state"))  # CI for density
 
-# D-hat = 1528 / ha
+# D-hat = 1528 / m
 # Survey area = 12 x 45 m 
 area <- 12 * 45 # 540 m^2
 
 # One hectare = 100 m x 100 m
 ha <- 100 * 100 # 10,000 m^2
 area/ha * 1505
-
-# No.glass beads placed on area = 178
-
-  # 136 collected 1st pass
-  # 25 2nd pass
 
